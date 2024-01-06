fbb05d7059174c77425f479d09524fcdac974268
KGB99 <kbirgi@gmail.com>
2024-01-06 20:24:03 +0100

    update for cluster

--- run_debugging_mac.sh
+++
@@ -1,1 +0,0 @@
-python3 train.py --workers 8 --epochs 2 --device cpu --batch-size 32 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
---
+++ scripts/cluster/train_trial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:24g
+#SBATCH --mem-per-cpu=40G
+#SBATCH --time=4:00:00
+module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
+python3 train.py --workers 8 --device 0 --batch-size 32 --data data/trial_custom.yaml --img 1280 1080 --cfg cfg/training/yolov7_medical.yaml --weights 'yolov7_training.pt' --name trial_ssd --hyp data/hyp.scratch.p5.yaml
