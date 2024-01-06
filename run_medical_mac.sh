bd56267118eda148fa9145144f80bf0177f35e54
KGB99 <k.birgi@gmail.com>
2024-01-06 14:23:20 +0100

    trials seem to be working

--- run_medical_mac.sh
+++ run_medical_mac.sh
@@ -1,1 +1,1 @@
-python3 train.py --workers 8 --epochs 2 --device cpu --batch-size 32 --data data/medical_custom.yaml --img 640 640 --cfg cfg/training/yolov7_medical.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+python3 train.py --workers 8 --epochs 10 --device cpu --batch-size 32 --data data/medical_custom.yaml --img 1280 1080 --cfg cfg/training/yolov7_medical.yaml --weights 'yolov7_training.pt' --name medical_trial --hyp data/hyp.scratch.p5.yaml
