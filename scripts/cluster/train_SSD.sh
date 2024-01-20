71727cf0909fd1eebe1cf7dc136d2f13ca5a0360
KGB99 <kbirgi@gmail.com>
2024-01-20 12:35:00 +0100

    updates to script and data

--- scripts/cluster/train_SSD.sh
+++ scripts/cluster/train_SSD.sh
@@ -3,7 +3,8 @@
 #SBATCH -n 1
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:24g
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=80G
 #SBATCH --time=24:00:00
 module load gcc/8.2.0 python_gpu/3.11.2 eth_proxy
-python3 train.py --workers 8 --device 0 --batch-size 16 --data data/medical_custom.yaml --img 640 640 --cfg cfg/training/yolov7_medical.yaml --weights 'yolov7_training.pt' --name medical_ssd --hyp data/hyp.scratch.p5.yaml
+python3 train.py --workers 8 --device 0 --batch-size 16 --epochs 3 --data data/medical_ssd.yaml --img 640 640 --cfg cfg/training/yolov7_medical.yaml --weights 'yolov7_training.pt' --name medical_ssd --hyp data/hyp.scratch.p5.yaml
+
